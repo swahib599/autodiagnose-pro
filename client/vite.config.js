a60a246a53df29507afac7e0085437b2eba8e3ad
swahib599 <shaib@DESKTOP-8M7378I.localdomain>
2025-09-13 18:50:39 +0300

    added DB

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5001',
-        changeOrigin: true
+        target: 'https://autodiagnose-pro-1.onrender.com',
+        changeOrigin: true,
+        secure: false
       }
     }
+  },
+  define: {
+    'process.env.BACKEND_URL': JSON.stringify('https://autodiagnose-pro-1.onrender.com')
   }
 })
